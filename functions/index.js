30e63e7358ee184f6e1b5a145b041c0782345fa0
coccoto <soprano.lune@gmail.com>
2020-03-24 14:46:30 +0900

    2020-03-24 14:46

--- functions/index.js
+++ functions/index.js
@@ -3,16 +3,37 @@ const FirebaseAdmin = require('firebase-admin')
 
 FirebaseAdmin.initializeApp()
 const database = FirebaseAdmin.firestore()
-const timestamp = FirebaseAdmin.firestore.FieldValue.serverTimestamp()
 
 exports.write = FirebaseFunctions.firestore
     .document('rooms/{roomId}/contents/{docId}')
     .onWrite((change, context) => {
+        const roomRef = database.collection('rooms')
+            .doc(context.params.roomId)
+            .collection('contents')
+            .doc(context.params.docId)
+
+        const date = new Date()
+        const diff = 0
+
+        const old = roomRef.orderBy('date', 'desc').limit(1).onSnapshot((snapshot) => {
+            snapshot.forEach((doc) => {
+                const ref = doc.data()
+
+                // 最終送信日時を取得する。
+                const date = ref.date
+
+                // 差分を取得する。
+                
+            })
+        })
+
         const document = database.collection('rooms')
             .doc(context.params.roomId)
             .collection('contents')
             .doc(context.params.docId)
 
-        
+        document.update({
+            timestamp: timestamp
+        })
     }
 )
